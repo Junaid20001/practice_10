4ea992b94de49a393b2cc10bf166501518d8c07b
Junaid Khalid <57348907+Junaid20001@users.noreply.github.com>
2023-06-23 23:30:48 +0500

    Add files via upload

---
+++ 10-Var_&_Dynamic/practice_10/practice_10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var keywords
+            /*
+            var num_name = "Khalid";
+            Console.WriteLine("my percentage is " + num_name + "% " + num_name.GetType());
+            num_name = "Junaid";
+            //num_name = 23;         //not work because of different data type
+            Console.WriteLine("my name is " + num_name + " & " + num_name.GetType());
+            Console.ReadLine(); 
+            */
+
+            //dynamic keywords
+            /*
+            dynamic num_name = 77;
+            Console.WriteLine("my percentage is " + num_name + "% " + num_name.GetType());
+            num_name = "Junaid"; 
+            Console.WriteLine("my name is " + num_name + " & " + num_name.GetType());
+            Console.ReadLine();
+            */
+
+        }
+    }
+}
